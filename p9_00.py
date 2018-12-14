d95e6c2d7b6994635e7f6a16530deec02090825f
M.Golovstik <mgolovstik@gmail.com>
2018-12-14 21:36:20 +0400

    Сделали так, что в файл пишется и сразу чиается из него

--- p9_00.py
+++ p9_00.py
@@ -1,4 +1,7 @@
+test = open('/home/mgolovstik/Python_Book/mytest.txt','w')
+test.write('jsdfhksdhbxdchfsj\n')                             
+test.close()
+
+
 test = open('/home/mgolovstik/Python_Book/mytest.txt')
-#test.write('bxdchfsj\n')                             
-#test.close()
 print(test.read())
